ac6a819f01d43f780cdc61b64e3041851c0d6edf
kelebekkadircan <kelebekkadircan@gmail.com>
2023-09-12 16:16:06 +0300

    Porfolio_Website

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
 import './App.css';
 import Intro from './components/intro/Intro';
 import About from './components/about/About';
 import ProductList from './components/productList/ProductList';
+import Contact from './components/contact/Contact';
+import Toggle from './components/toggle/Toggle';
+// import { ThemeProvider } from './context';
+import { ThemeContext } from './context.js'
 
 function App() {
+
+  const theme = useContext(ThemeContext)
+
+  const darkMode = theme.state.darkMode;
+
+
+
   return (
-    <Fragment>
-      <div>
+    <>
+      <div style={{ backgroundColor: darkMode ? "#222" : "white", color: darkMode ? "white" : "black" }}>
+
+        <Toggle />
         <Intro />
         <About />
         <ProductList />
+        <Contact />
+
       </div>
 
-    </Fragment>
+    </>
   );
 }
 
